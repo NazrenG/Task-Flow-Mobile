21620fdef084e63875909f569a6a5eae6d317d2d
Nəzrin Quliyeva <140939317+NazrenG@users.noreply.github.com>
2025-06-05 21:13:08 +0400

    Initial commit Generated by create-expo-app 3.4.2.

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  // NOTE: Update this to include the paths to all of your component files.
+  content: ["./app/**/*.{js,jsx,ts,tsx}"],
+  presets: [require("nativewind/preset")],
+  theme: {
+    extend: {
+      colors: {
+        customYellow: "rgb(255,188,3, 0.3)",
+      },
+      fontFamily: {
+        sans: ["Inter", "sans-serif"],
+        serif: ["Merriweather", "serif"],
+      },
+    },
+  },
+  plugins: [],
+};
