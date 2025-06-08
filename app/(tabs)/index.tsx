2a63ef95d18f8a8910ddb1f82ba62b29022d2ce3
Nəzrin Quliyeva <140939317+NazrenG@users.noreply.github.com>
2025-06-08 16:06:17 +0400

    quiz page

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,102 +1,14 @@
-import { FontAwesome6, MaterialCommunityIcons } from "@expo/vector-icons";
-import { LinearGradient } from "expo-linear-gradient";
 
-import { Text, View } from "react-native";
-
+import { Redirect } from "expo-router";
 export default function HomeScreen() {
   return (
-    <View
-      className={
-        "flex-1 w-full flex-row items-start justify-between bg-background p-4"
-      }
-    >
-      <View
-        className={
-          "flex-1 flex-row  items-center justify-between bg-white px-3 py-5 rounded-lg shadow-lg gap-2"
-        }
-      >
-        <View
-          className={
-            "flex-1   gap-2 justify-between bg-[#caf8e7] p-2  rounded-lg shadow-lg"
-          }
-        >
-          <View className="flex-row items-center gap-2">
-            <LinearGradient
-              colors={["#059669", "#34D399"]}
-              style={{
-                width: 30,
-                height: 30,
-                borderRadius: 9999,
-                shadowColor: "#000",
-                shadowOffset: { width: 0, height: 2 },
-                shadowOpacity: 0.3,
-                shadowRadius: 4,
-                justifyContent: "center",
-                alignItems: "center",
-              }}
-            >
-              <FontAwesome6 name="bell" size={15} color="white" />
-            </LinearGradient>
-            <Text className="color-white font-extrabold text-xl">9</Text>
-          </View>
-
-          <Text className="text-black font-bold">Messages</Text>
-        </View>
-        <View
-          className={
-            "flex-1   gap-2 justify-between bg-[#f8ebc5] p-2  rounded-lg shadow-lg"
-          }
-        >
-          <View className="flex-row items-center gap-2">
-            <LinearGradient
-              colors={["#FFBC03", "#fbd56f"]}
-              style={{
-                width: 30,
-                height: 30,
-                borderRadius: 9999,
-                shadowColor: "#000",
-                shadowOffset: { width: 0, height: 2 },
-                shadowOpacity: 0.3,
-                shadowRadius: 4,
-                justifyContent: "center",
-                alignItems: "center",
-              }}
-            >
-              <MaterialCommunityIcons name="bell" size={15} color="white" />
-            </LinearGradient>
-            <Text className="color-white font-extrabold  text-xl">9</Text>
-          </View>
-
-          <Text className="text-black font-bold">Notification</Text>
-        </View>
-        <View
-          className={
-            "flex-1   gap-2 justify-between bg-[#c2b8ff] p-2  rounded-lg shadow-lg"
-          }
-        >
-          <View className="flex-row items-center gap-2">
-            <LinearGradient
-              colors={["#3C21F7", "#7661fd"]}
-              style={{
-                width: 30,
-                height: 30,
-                borderRadius: 9999,
-                shadowColor: "#000",
-                shadowOffset: { width: 0, height: 2 },
-                shadowOpacity: 0.3,
-                shadowRadius: 4,
-                justifyContent: "center",
-                alignItems: "center",
-              }}
-            >
-              <MaterialCommunityIcons name="calendar" size={15} color="white" />
-            </LinearGradient>
-            <Text className="color-white font-extrabold text-xl">9</Text>
-          </View>
-
-          <Text className="text-black font-bold">Calendar</Text>
-        </View>
-      </View>
-    </View>
+    // <View
+    //   className={
+    //     "flex-1 w-full flex-row items-start justify-between bg-background p-4"
+    //   }
+    // >
+    //   <CountView />
+    // </View>
+    <Redirect href="/quiz"/> 
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
 /** @type {import('tailwindcss').Config} */
-import { Colors } from "./constants/Colors";
+import {Colors} from "./constants/Colors.ts"
 module.exports = {
   // NOTE: Update this to include the paths to all of your component files.
   content: ["./app/**/*.{js,jsx,ts,tsx}"],
@@ -12,6 +12,9 @@ module.exports = {
         navyBlue: `${Colors.primary.navyBlue}`,
         green: `${Colors.primary.green}`,
         darkPurple: `${Colors.primary.darkPurple}`,
+        bg_violet: Colors.secondary.bg_violet, 
+        bg_green: Colors.secondary.bg_green,
+        bg_yellow: Colors.secondary.bg_yellow,
       },
 
       fontFamily: {
