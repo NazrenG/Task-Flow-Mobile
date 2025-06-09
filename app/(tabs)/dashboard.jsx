bc6f5d759d8d34210189aa7370dddff7b1999769
Nəzrin Quliyeva <140939317+NazrenG@users.noreply.github.com>
2025-06-09 23:21:53 +0400

    feat: add language and navbar

--- app/(tabs)/dashboard.jsx
+++ app/(tabs)/dashboard.jsx
@@ -1,16 +1,20 @@
-import { Dimensions, View } from "react-native";
-import CountView from "@/components/CountView";
-export default function Dashboard() {
-  const width = Dimensions.get("window").width;
+import React, { useState } from "react";
+import { View, Text, Dimensions } from "react-native";
+import Header from "@/components/Header"; 
+import { useTranslation } from "react-i18next";
 
-  const height = Dimensions.get("window").height;
+export default function Dashboard() {
+  const { t } = useTranslation();
+  const [searchText, setSearchText] = useState("");
 
   return (
-    <View
-      className="flex-1 items-center justify-between   bg-background p-5 "
-      style={{ width, height, paddingTop: 80 }}
-    > 
-      <CountView />
+     <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+     
+         <Header onSearch={setSearchText} />
+     <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+        <Text style={{ fontSize: 24 }}>{t("welcome")}</Text> 
+         <Text style={{ color: "black" }}>Dashboard Screen</Text>
+      </View>
     </View>
   );
 }
