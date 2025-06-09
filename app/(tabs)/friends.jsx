3599ec768d6cd326c99d53c1c46717eb20f68066
Sevgi <sevgi.elesgerova@gmail.com>
2025-06-09 23:37:53 +0400

    first commit

--- app/(tabs)/friends.jsx
+++ app/(tabs)/friends.jsx
@@ -1,9 +1,9 @@
-import { View, Text } from "react-native";
+import { View } from "react-native";
 
 export default function Friends() {
   return (
     <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <Text style={{ color: "black" }}>Friend Screen</Text>
+      <View className="bg-red size-4"></View>
     </View>
   );
 }
