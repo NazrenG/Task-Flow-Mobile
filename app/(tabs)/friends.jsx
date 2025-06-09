86522443ec768f6019f33e57e7f16681fe079590
MemmedzadeZ <zehramemmedzade438@gmail.com>
2025-06-10 01:58:33 +0400

    commit

--- app/(tabs)/friends.jsx
+++ app/(tabs)/friends.jsx
@@ -1,23 +1,12 @@
 import { View, Text } from "react-native";
-<<<<<<< HEAD
-import Login from "../auth/login";
-
-=======
 import Header from "../../components/Header";
 import { useTranslation } from "react-i18next";
 import { useState } from "react";
->>>>>>> 4460fe2c91271780dce008b7102d199ad9396d44
 export default function Friends() {
    const { t } = useTranslation();
   const [searchText, setSearchText] = useState("");
 
   return (
-<<<<<<< HEAD
-    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-
-      <Text style={{ color: "black" }}>Friend Screen</Text>
-    </View>
-=======
      <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
   
            <Header onSearch={setSearchText} />
@@ -26,6 +15,5 @@ export default function Friends() {
                 <Text style={{ color: "black" }}>Friend Screen</Text>
         </View>
       </View>
->>>>>>> 4460fe2c91271780dce008b7102d199ad9396d44
   );
 }
