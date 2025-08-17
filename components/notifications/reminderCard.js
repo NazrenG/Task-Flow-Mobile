a83ee806b5709db4977b3e95a982a00ff2437e3e
Nəzrin Quliyeva <140939317+NazrenG@users.noreply.github.com>
2025-08-17 20:02:12 +0400

    add friend and notification fetch

--- components/notifications/reminderCard.js
+++ components/notifications/reminderCard.js
@@ -1,7 +1,16 @@
 import { FontAwesome } from "@expo/vector-icons";
 import { Text, TouchableOpacity, View } from "react-native";
+import { fetchDeleteReminterNotification } from "../../utils/notificationUtils";
 
 export default function ReminderCard({ date, message }) {
+  const handleDelete = async () => {
+    try {
+      await fetchDeleteReminterNotification(date);
+      console.log("Reminder deleted");
+    } catch (error) {
+      console.error("Error deleting reminder:", error);
+    }
+  };
   return (
     <View className="flex-row justify-between items-center bg-gray-50 p-3 rounded-lg">
       <View>
@@ -9,7 +18,7 @@ export default function ReminderCard({ date, message }) {
         <Text className="text-gray-600">{message}</Text>
       </View>
       <TouchableOpacity
-        onPress={() => console.log("Deleted")}
+        onPress={handleDelete}
         className="p-2 bg-red-100 rounded-full"
       >
         <FontAwesome name="close" size={15} color="red" />
